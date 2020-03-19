882456734e1fbb0df41c9f69c8147363237b633b
李岚琪 <syaoran_lee7@163.com>
2020-03-19 16:20:29 +0800

    二次曲面

--- src/views/math/QuadraticSurface/QuadraticSurface.js
+++ src/views/math/QuadraticSurface/QuadraticSurface.js
@@ -1,13 +1,13 @@
 import MegaMath from "@/utils/MegaMath.js";
 
-const interval = 1; // 截平面间隔
 const funcs = {
-    getDataSets (range, params) {
+    getDataSets (range, inter, params) {
         /* 基本思路如下 */
         /* 先在x轴原点两侧选取以interval为单位, total一半为数量的截平面, 即确定x的值 */
         /* 同理确定y轴截平面, 即确定y值 */
         /* 最后通过解二元一次方程确定z的值 */
         const r = Number(range);
+        const interval = Number(inter);
         const res = [];
         const a = params.split(",");
 
@@ -26,10 +26,27 @@ const funcs = {
                 const QUADRATIC = Number(a[2]); // 二次项
                 const SINGLE = (Number(a[4]) * y) + (Number(a[5]) * x) + Number(a[8]); // 一次项
                 const CONSTANT = A11 + A22 + A12 + A1 + A2 + A4; // 常数项
-                const z = MegaMath.solveQuadraticEquation(QUADRATIC, SINGLE, CONSTANT);
-                z.forEach(i => {
-                    res.push([x, y, i]);
-                });
+
+                if (QUADRATIC) {
+                    /* 有二次项和一次项才需要解一元二次方程 */
+                    const z = MegaMath.solveQuadraticEquation(QUADRATIC, SINGLE, CONSTANT);
+                    z.forEach(i => {
+                        res.push([x, y, i]);
+                    });
+                } else if (!QUADRATIC && SINGLE) {
+                    /* 没有二次项则是一元一次方程 */
+                    res.push([x, y, -1 * CONSTANT / SINGLE]);
+                } else if (!QUADRATIC && !SINGLE && A11 + A22 + A12 + A1 + A2 + A4 === 0) {
+                    /* 此时z可以取任何值, 但需要验证x与y能否满足给定方程 */
+                    for (let z = -1 * r; z <= r; z = z + interval) {
+                        res.push([x, y, z]);
+                    }
+                }
+
+                // console.log("==========================");
+                // console.log("x:", x);
+                // console.log("y:", y);
+                // console.log("一元二次方程:", QUADRATIC + "z²+" + SINGLE + "z+" + CONSTANT + "=0");
             }
         }
         return res;
--- src/views/math/QuadraticSurface/QuadraticSurface.vue
+++ src/views/math/QuadraticSurface/QuadraticSurface.vue
@@ -19,6 +19,9 @@
                     <el-form-item label="显示范围" required>
                         <el-input v-model="range" type="text"></el-input>
                     </el-form-item>
+                    <el-form-item label="精细度" required>
+                        <el-input v-model="interval" type="text"></el-input>
+                    </el-form-item>
                     <el-form-item label="参数" required>
                         <el-tooltip
                             effect="dark"
@@ -30,11 +33,26 @@
                         </el-tooltip>
                         <el-input v-model="params" type="text"></el-input>
                     </el-form-item>
+                    <el-form-item label="方程提示" required>
+                        <el-select v-model="type">
+                            <el-option label="球面" value="sphere"></el-option>
+                            <el-option label="圆柱面" value="cylinder"></el-option>
+                            <el-option label="椭圆抛物面" value="EllipticalParaboloid"></el-option>
+                            <el-option label="双曲抛物面" value="HyperbolicParaboloid"></el-option>
+                            <el-option label="单叶双曲面" value="Hyperboloid"></el-option>
+                            <el-option label="双叶双曲面" value="hyperboloidOfTwoSheets"></el-option>
+                            <el-option label="椭圆锥面" value="EllipticCone"></el-option>
+                        </el-select>
+                    </el-form-item>
 
                 </el-form>
                 <el-button type="primary" @click="initData">
                     生成图像
                 </el-button>
+                <div class="tips">
+                    <span class="name">{{ this.typeName }}</span>的一般方程是: <span class="equation">{{ this.equation }}</span>
+                </div>
+                <div class="example">{{ this.example }}</div>
             </div>
 
             <div class="setting-result">
@@ -53,18 +71,109 @@ export default {
     data () {
         return {
             inputData: {},
-            range: 100, // 形成数据的范围
-            params: "1,1,1,0,0,0,-4,-4,-4,0",
+            range: 50, // 形成数据的范围
+            interval: 5, // 精细度
+            params: "1,1,1,0,0,0,-4,-4,-4,0", // 参数
+            type: "cylinder", // 方程形式
 
             dataSets: [] // 数据集
         };
     },
 
+    computed: {
+        typeName () {
+            let name = "";
+            switch (this.type) {
+                case "cylinder":
+                    name = "圆柱面";
+                    break;
+                case "sphere":
+                    name = "球面";
+                    break;
+                case "EllipticalParaboloid":
+                    name = "椭圆抛物面"
+                    break;
+                case "HyperbolicParaboloid":
+                    name = "双曲抛物面";
+                    break;
+                case "Hyperboloid":
+                    name = "单叶双曲线";
+                    break;
+                case "hyperboloidOfTwoSheets":
+                    name = "双叶双曲面";
+                    break;
+                case "EllipticCone":
+                    name = "椭圆锥面";
+                    break;
+                default:
+                    name = "";
+            }
+            return name;
+        },
+        equation () {
+            let e = "";
+            switch (this.type) {
+                case "cylinder":
+                    e = "X² + Y² - R² = 0";
+                    break;
+                case "sphere":
+                    e = "X² + Y² + Z² - R² = 0";
+                    break;
+                case "EllipticalParaboloid":
+                    e = "X²/a² + Y²/b² - 2Z = 0";
+                    break;
+                case "HyperbolicParaboloid":
+                    e = "-X²/a² + Y²/b² - 2Z = 0";
+                    break;
+                case "Hyperboloid":
+                    e = "X²/a² + Y²/b² - Z²/c² - 1 = 0";
+                    break;
+                case "hyperboloidOfTwoSheets":
+                    e = "X²/a² + Y²/b² - Z²/c² + 1 = 0";
+                case "EllipticCone":
+                    e = "X²/a² + Y²/b² - Z² = 0"
+                default:
+                    e = "";
+            }
+            return e;
+        },
+        example () {
+            let e = "";
+            switch (this.type) {
+                case "cylinder":
+                    e = "1,1,0,0,0,0,0,0,0,-16";
+                    break;
+                case "sphere":
+                    e = "1,1,1,0,0,0,-2,-2,-2,-300";
+                    break;
+                case "EllipticalParaboloid":
+                    e = "0.2,0.2,0,0,0,0,0,0,-2,0";
+                    break;
+                case "HyperbolicParaboloid":
+                    e = "-0.2,0.2,0,0,0,0,0,0,-2,0";
+                    break;
+                case "Hyperboloid":
+                    e = "0.1,0.2,-0.05,0,0,0,0,0,0,-1";
+                    break;
+                case "hyperboloidOfTwoSheets":
+                    e = "0.1,0.2,-0.05,0,0,0,0,0,0,1";
+                    break;
+                case "EllipticCone":
+                    e = "0.33,0.5,-1,0,0,0,0,0,0,0";
+                    break;
+                default:
+                    e = "";
+            }
+            return "参数例子: " + e;
+        }
+    },
+
     methods: {
         // 生成数据
         initData () {
-            this.dataSets = helper.getDataSets(this.range, this.params);
+            this.dataSets = helper.getDataSets(this.range, this.interval, this.params);
             this.initOriginEchart();
+            console.log("dataSets:", this.dataSets);
         },
 
         // 渲染原始数据
@@ -108,7 +217,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/styles/variables.scss";
 .lan-main {
+    .setting-base {
+        .tips {
+            margin-top: 20px;
+            .name { color: $fontColor_blue; font-weight: 700; }
+            .equation { font-weight: 700; }
+        }
+        .example { margin-top: 20px; }
+    }
     .result {
         display: flex;
         #geometry {
