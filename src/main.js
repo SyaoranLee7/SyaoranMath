e94a4f26c275347d1120efdae72486efb08bf6f8
李岚琪 <syaoran_lee7@163.com>
2020-04-03 10:54:54 +0800

    添加logger

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
 import Vue from "vue";
 
+import "@/styles/index.scss"; // global css
+import "normalize.css/normalize.css"; // a modern alternative to CSS resets
+import "echarts-gl"; // Echarts
+import logger from "@/utils/logger"; // 替换console
+
 // ElementUI
 import Element from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 Vue.use(Element);
 
-// Echarts
-import "echarts-gl";
+const install = function (VueClass, opts = {}) {
+    if (install.installed) return;
 
-import "normalize.css/normalize.css"; // a modern alternative to CSS resets
-
-import "@/styles/index.scss"; // global css
+    /* methods */
+    VueClass.logger = logger;
+    VueClass.prototype.$logger = logger;
+};
+Vue.use(install);
 
 import App from "./App";
 import router from "./router";
